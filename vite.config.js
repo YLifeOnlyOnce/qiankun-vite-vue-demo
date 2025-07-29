fd9475c9096eb9f0eb595fae895627b8058c6be7
yaoxiangbo <yaoxiangbo@qianxin.com>
2025-07-29 15:50:08 +0800

    chore: name change

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import { name as appName } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 插件vite-plugin-qiankun在生产模式下依旧不支持publicPath, 需要将vite.config.js中base配置写死。导致多环境部署不便捷。无法像在webpack结合window.INJECTED_PUBLIC_PATH_BY_QIANKUN + publicpath来解决。
+  publicPath:`/microapps/${appName}/`,
+  base: `/microapps/${appName}`,
   plugins: [
     vue(),
     qiankun(appName, {
